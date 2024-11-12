8e2675367dc22aae1ce45e2f3be3c82635af58d7
John Rey Quidet (vox402) <vox402@mail.usask.ca>
2024-11-12 15:19:50 -0600

    my game
    import game

---
+++ killzone.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	print("You died")
+	Engine.time_scale = 0.5
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+	get_tree().reload_current_scene()
